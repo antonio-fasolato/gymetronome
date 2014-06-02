0e93c682e184113afa96357b02fe80b088a055ec
OpenShift guest <antonio.fasolato@gmail.com>
2014-06-02 16:34:11 -0400

    Add function corrected

--- js/index.js
+++ js/index.js
@@ -1,22 +1,46 @@
+var timers = [];
+var currentIndex = null;
+
 var app = {
-    timers: [],
-    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
 
     bindEvents: function() {
+    
         $("#btnAdd").click(this.btnAddClicked);
+        $("#btnClear").click(this.btnClearClicked);
+        $("#btnStart").click(this.btnStartClicked);
     },
 
     btnAddClicked: function () {
         var d = $("#timerBase").clone();
-        var x = this.timers.push(d);
+        
+        var x = timers.push(d);
         d.attr("data-array-index", x - 1);
+        d.attr("data-initial-value", $("#tbTime").val());
         d.children(".timer").text($("#tbTime").val());
         d.appendTo("#tblTimers");
         d.show(500);
         $("#tbTime").val("");
-    }
+    },
+    
+    btnClearClicked: function() {
+      $.each(timers, function(index, value) {
+        if(value !== null) {
+            value.hide(500, function() {
+                value.remove();
+            });
+            timers[index] = null;
+        }
+      });
+    },
+    
+    btnStartClicked: function() {
+        if(currentIndex === null) {
+            currentIndex = 0;
+            this.startTimer();
+        }
+    },
 };
