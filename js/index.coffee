8933ec70270b143c2e8611dec150ae0baba95905
antonio-fasolato <antonio.fasolato@gmail.com>
2014-06-03 10:39:20 +0200

    pulsante start inziato

--- js/index.coffee
+++ js/index.coffee
@@ -10,7 +10,7 @@ root.app =
     bindEvents : ->
         $("#btnAdd").click(this.btnAddClicked)
         $("#btnClear").click(this.btnClearClicked)
-        #$("#btnStart").click(this.btnStartClicked)
+        $("#btnStart").click(this.btnStartClicked)
 
     btnAddClicked: ->
           d = $("#timerBase").clone()
@@ -26,52 +26,8 @@ root.app =
     btnClearClicked: ->
         (t.hide 500 if t != null) for t in timers
 
-
-###
-var timers = [];
-var currentIndex = null;
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-
-    bindEvents: function() {
-    
-        $("#btnAdd").click(this.btnAddClicked);
-        $("#btnClear").click(this.btnClearClicked);
-        $("#btnStart").click(this.btnStartClicked);
-    },
-
-    btnAddClicked: function () {
-        var d = $("#timerBase").clone();
-        
-        var x = timers.push(d);
-        d.attr("data-array-index", x - 1);
-        d.attr("data-initial-value", $("#tbTime").val());
-        d.children(".timer").text($("#tbTime").val());
-        d.appendTo("#tblTimers");
-        d.show(500);
-        $("#tbTime").val("");
-    },
-    
-    btnClearClicked: function() {
-      $.each(timers, function(index, value) {
-        if(value !== null) {
-            value.hide(500, function() {
-                value.remove();
-            });
-            timers[index] = null;
-        }
-      });
-    },
-    
-    btnStartClicked: function() {
-        if(currentIndex === null) {
-            currentIndex = 0;
-            this.startTimer();
-        }
-    },
-};
-###
+    btnStartClicked: ->
+        if root.currentIndex == null
+            root.currentIndex = 0;
+            d = root.timers[root.currentIndex]
+            alert d.children(".timer").text()
--- js/index.js
+++ js/index.js
@@ -13,7 +13,8 @@
     },
     bindEvents: function() {
       $("#btnAdd").click(this.btnAddClicked);
-      return $("#btnClear").click(this.btnClearClicked);
+      $("#btnClear").click(this.btnClearClicked);
+      return $("#btnStart").click(this.btnStartClicked);
     },
     btnAddClicked: function() {
       var d, x;
@@ -34,57 +35,15 @@
         _results.push(t !== null ? t.hide(500) : void 0);
       }
       return _results;
+    },
+    btnStartClicked: function() {
+      var d;
+      if (root.currentIndex === null) {
+        root.currentIndex = 0;
+        d = root.timers[root.currentIndex];
+        return alert(d.children(".timer").text());
+      }
     }
   };
-
-  /*
-  var timers = [];
-  var currentIndex = null;
-  
-  var app = {
-      // Application Constructor
-      initialize: function() {
-          this.bindEvents();
-      },
-  
-      bindEvents: function() {
-      
-          $("#btnAdd").click(this.btnAddClicked);
-          $("#btnClear").click(this.btnClearClicked);
-          $("#btnStart").click(this.btnStartClicked);
-      },
-  
-      btnAddClicked: function () {
-          var d = $("#timerBase").clone();
-          
-          var x = timers.push(d);
-          d.attr("data-array-index", x - 1);
-          d.attr("data-initial-value", $("#tbTime").val());
-          d.children(".timer").text($("#tbTime").val());
-          d.appendTo("#tblTimers");
-          d.show(500);
-          $("#tbTime").val("");
-      },
-      
-      btnClearClicked: function() {
-        $.each(timers, function(index, value) {
-          if(value !== null) {
-              value.hide(500, function() {
-                  value.remove();
-              });
-              timers[index] = null;
-          }
-        });
-      },
-      
-      btnStartClicked: function() {
-          if(currentIndex === null) {
-              currentIndex = 0;
-              this.startTimer();
-          }
-      },
-  };
-  */
-
 
 }).call(this);
